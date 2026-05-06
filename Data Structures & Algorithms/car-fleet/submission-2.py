9f1fd3e2b84a8103f02e77a721f0aad5cc25d4d2
Raiyan Rubaiyyat <134195180+RaiyanRT@users.noreply.github.com>
2026-05-06 12:40:42 +1000

    Add: car-fleet - submission-2

---
+++ Data Structures & Algorithms/car-fleet/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        cars = []
+        stack = []
+
+
+        # Compute the times of each car
+        for i in range(len(position)):
+            time = (target - position[i]) / speed[i]
+            cars.append((position[i], time))
+        # sort cars by position will make time complexity O Log N
+        cars.sort(reverse=True)
+
+        # If stack empty append whatever time as its sorted by position from biggest to shortest. 
+        # Then check their timings, and if their timings is greater append that to the stack as that would be a seperate fleet.
+        for position, time in cars:
+            if not stack:
+                stack.append(time)
+            if time > stack[-1]:
+                stack.append(time)
+
+        return len(stack)
